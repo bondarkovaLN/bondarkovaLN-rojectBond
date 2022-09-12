e577931d984050f483f1e1d8603b1e16be17efc4
Bondarkova Lubov <xriss@yandex.ru>
2022-09-12 12:49:30 +0200

    решение практической 1 файлы в папке Pr1

---
+++ Pr1/P2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("введи первое число:");
+int num1 = Convert.ToInt32(Console . ReadLine ());
+
+Console.WriteLine("введи второе число:");
+int num2 = Convert.ToInt32(Console . ReadLine ());
+
+Console.WriteLine("введи третье число:");
+int num3 = Convert.ToInt32(Console . ReadLine ()); 
+
+if (num1>num2)
+{
+    
+Console.WriteLine(num1);
+
+}
+else
+{
+    Console.WriteLine((num2)); 
+    
+    }
+   if (num1>num3)
+{
+    
+Console.WriteLine("максимальное число-",num1);
+ Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine("максимальное-",(num3)); 
+    Console.Write(num3);
+}
+   
