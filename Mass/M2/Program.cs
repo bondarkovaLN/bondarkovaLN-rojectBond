12fc6377696474a7beaa52e050f884f45d291622
Bondarkova Lubov <xriss@yandex.ru>
2022-09-13 08:11:42 +0200

    решение практической 2 задача 13  файлы в папке M2

---
+++ Mass/M2/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine("Введите трехзначное число:");
+ int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+Console.Write(k < 0 ? "->третьей цифры нет" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+
+Console.ReadLine();
