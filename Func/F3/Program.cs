caaa8e7a3bedd6de63be4ea020a37897a953b839
Bondarkova Lubov <xriss@yandex.ru>
2022-09-13 09:15:10 +0200

    решение практической 3  задача 23  файлы в папке F3

---
+++ Func/F3/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void Degree(string num)
+{
+     int [] thirdNumber = new int[Convert.ToInt32(num)];
+   int count = 0;
+   while (count < Convert.ToInt32(num))
+   {
+        thirdNumber[count] = (count + 1) * (count + 1) * (count + 1);
+        count ++;
+   }
+  
+    count = 0;
+    while (count < thirdNumber.Length)
+    {
+        Console.Write(thirdNumber[count]);
+        count ++;
+       
+        if (count < thirdNumber.Length)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+
+
+Console.Write("Введите натуральное число от 1 до 1000: ");
+string numb = Console.ReadLine();
+
+if ( Convert.ToInt32(numb) >= 1 && Convert.ToInt32(numb) <= 1000 ) 
+{
+     Degree(numb);
+}
+else
+{
+    Console.WriteLine("Вы ввели не допустимое число!");
+}
